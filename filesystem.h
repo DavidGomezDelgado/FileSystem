8f4667b11f366ea8f14d7def7e5064cc8f21d923
davidgomezdelgado <12401dagode@uma.es>
2023-05-31 14:37:49 +0200

    Mejora sistema

--- filesystem.h
+++ filesystem.h
@@ -1,105 +1,99 @@
-
 #define N_DIRECTOS 10
 //#define N_SIMPLES  1
 //#define N_DOBLES 1
 //#define N_TRIPLES 1
+
+#define NUM_BLOCKS 1048576 //(4GB) = 1048576 * 4096
+#define NUM_INODES 16777216 //1073741824÷64
+
 #define BLOCK_SIZE 4096
 #define MAGIC_N 123456
 
-struct directory_entry{
-	char name[24];
+
+typedef struct directory_entry{
+	char name[28];
 	struct inode_fs *inode;
 };
+
+
 
 struct inode_fs{
 	int i_num;
 	char i_type;
 	int i_tam;
 	int i_permisos;
+	int i_links;
 	int i_directos[N_DIRECTOS]; //320 direcciones (32 bloques cada puntero)
+	//offset en el fichero?
+	char relleno[8]; //Bytes de relleno para tener un múltiplo del tamaño del bloque
 };
 
-// typedef uint8_t[][BLOCKSIZE] block_t;
-typedef uint8_t block_t[BLOCK_SIZE];
 
-struct bitmap_t {
-	uint8_t *bitmap;
+//estructura que reutilizamos para bitmapb y bitmapi
+typedef struct bitmap_t{
+	unsigned char *array;
 	uint64_t size;
 };
 
-struct superblock_fs {
-	uint64_t magic_number;
-	uint64_t block_size;
-	uint64_t num_blocks;
-	uint64_t num_inodes;
-	// uint64_t num_blocks_libres;
-	// uint64_t num_inodes_libres;
-
-	uint64_t bitmapb_reserved_blocks;
-	uint64_t bitmapi_reserved_blocks;
-	uint64_t inode_reserved_blocks;
-	uint64_t reserved_blocks;
-
-	off_t bitmapb_offset;
-	off_t bitmapi_offset;
-	off_t offset_inodos;
-	off_t offset_bloques;
-};
+//structura bloque de datos
+//bloque con datos
+typedef uint8_t block_t[BLOCK_SIZE];
 
 typedef struct {
-	struct superblock_fs *sb;
-	struct bitmap_t block_bitmap;
+	struct superblock_fs *superblock;
 	struct bitmap_t inode_bitmap;
-	struct inode_fs *inodes;
-//	struct uint8_t *block;
-	block_t *blocks;
+	struct bitmap_t block_bitmap;
+	struct inode_fs *inode;
+	//array incompleto que nos dice en cuál bloque de datos estamos
+	block_t *block;
+	int fd;
 } filesystem_t;
 
-filesystem_t *myfilesystem;
 
-/*
-//BITMAP DE BLOQUES
-struct block_bitmap_fs{
-	unsigned char bitmap[NUM_BLOCKS/8]; // Se divide entre 8 porque cada byte tiene 8 bits, por lo que sería un array de 125 bytes-
-	uintptr_t map[NUM_BLOCKS];
+struct superblock_fs {
+	unsigned long magic_number;
+	unsigned long bitmapb_offset;
+	unsigned long bitmapi_offset;
+	unsigned long free_blocks;
+	unsigned long free_inodes;
+    unsigned long num_blocks;
+    unsigned long num_inodes;
+    unsigned long offset_inodos;
+    unsigned long offset_bloques;
+    unsigned long block_size;
+    unsigned long inode_size;
 };
 
-//BITMAP DE INODOS
-struct inode_bitmap_fs{
-	unsigned char bitmap[NUM_INODES / 8];
-};
-*/
 
 /***************************************************************
  * 							FUNCTIONS						   *
  ***************************************************************/
  
-//bitmap.c
-// int free_inode(struct inode_bitmap_fs *);
-int free_inode(struct filesystem_t *);
-// int free_block(struct block_bitmap_fs *);
-int free_block(struct filesystem_t *);
- 
-//create_inode.c
-struct inode_fs *create_inode(char, char*, struct inode_bitmap_fs *, struct block_bitmap_fs *);
-struct inode_fs *create_root(struct inode_bitmap_fs *, struct block_bitmap_fs *);
- 
-//file_manager.c
-void touch (char *, char, char *, struct inode_fs *, struct inode_bitmap_fs *, struct block_bitmap_fs *);
-void print_directory(struct inode_fs);
-void clean_inode(struct inode_fs *, struct block_bitmap_fs *);
- 
-//tree_manager.c
-struct inode_fs *inode_search(char *, struct inode_fs *, struct block_bitmap_fs *);
-struct inode_fs *_inode_search(char *, char *, struct inode_fs, struct block_bitmap_fs);
+ //bitmap.c
+unsigned long free_bit(struct bitmap_t *);
 
-//file_remove.c
-void rm (char *, struct inode_fs *, struct inode_bitmap_fs *, struct block_bitmap_fs *);
-
-//file_operations.c
-void file_edit(char *, char *, struct inode_fs *, struct block_bitmap_fs *);
-char *read_file(char *,struct inode_fs *, struct block_bitmap_fs *);
-void rename_file(char *, char *, struct inode_fs *, struct block_bitmap_fs* );
-
-//directory_operations.c
-char *read_directory(char *,struct inode_fs *, struct block_bitmap_fs *);
+ 
+ //create_inode.c
+// struct inode_fs *create_inode(char, char*, struct inode_bitmap_fs *, struct block_bitmap_fs *);
+// struct inode_fs *create_root(struct inode_bitmap_fs *, struct block_bitmap_fs *);
+//
+// //file_manager.c
+// void touch (char *, char, char *, struct inode_fs *, struct inode_bitmap_fs *, struct block_bitmap_fs *);
+// void print_directory(struct inode_fs);
+// void clean_inode(struct inode_fs *, struct block_bitmap_fs *);
+//
+// //tree_manager.c
+// struct inode_fs *inode_search(char *, struct inode_fs *, struct block_bitmap_fs *);
+// struct inode_fs *_inode_search(char *, char *, struct inode_fs, struct block_bitmap_fs);
+//
+// //file_remove.c
+// void rm (char *, struct inode_fs *, struct inode_bitmap_fs *, struct block_bitmap_fs *);
+//
+// //file_operations.c
+// void file_edit(char *, char *, struct inode_fs *, struct block_bitmap_fs *);
+// char *read_file(char *,struct inode_fs *, struct block_bitmap_fs *);
+//
+//
+//
+// //directory_operations.c
+// char *read_directory(char *,struct inode_fs *, struct block_bitmap_fs *);
--- mkFileSystem.c
+++ mkFileSystem.c
@@ -8,32 +8,12 @@
 #include <sys/types.h>
 #include "filesystem.h"
 
-#define MAGIC_N 123456
-
-//structura bloque de datos
-typedef char block_t[][BLOCK_SIZE];
-
-struct superblock_fs {
-	unsigned long magic_number;
-	unsigned long bitmapb_offset;
-	unsigned long bitmapi_offset;
-    unsigned long num_blocks;
-    unsigned long num_inodes;
-    unsigned long offset_inodos;
-    unsigned long offset_bloques;
-    unsigned long block_size;
-};
-
 int main(int argc, char *argv[]){
 	int file;
 	struct stat statbuf;
-	struct superblock_fs *superblock;
-	struct bitmap_t bitmapb;
-	struct bitmap_t bitmapi;
-	struct inode_fs *inodos;
-	struct block_t *bloques;
-	unsigned long nb = 1;
-	int pagesize = sysconf(_SC_PAGE_SIZE);
+	//private_data private_data = malloc(sizeof(filesystem_t));
+	filesystem_t myfilesystem;
+	//int pagesize = sysconf(_SC_PAGE_SIZE);
 
 	//file = open(argv[1], O_RDWR);
 	file = open("filesystem.img", O_RDWR);
@@ -46,93 +26,99 @@ int main(int argc, char *argv[]){
 		exit(EXIT_FAILURE);
 	}
 
-	superblock = mmap(NULL,sizeof(struct superblock_fs),PROT_WRITE|PROT_READ, MAP_SHARED,file, 0);
-	 if (superblock == MAP_FAILED) {
+	myfilesystem.superblock = mmap(NULL,sizeof(struct superblock_fs),PROT_WRITE|PROT_READ, MAP_SHARED,file, 0);
+	 if (myfilesystem.superblock == MAP_FAILED) {
 		 perror("Error al mapear el archivo");
 		 close(file);
 		 exit(EXIT_FAILURE);
 	 }
-	 superblock->magic_number = MAGIC_N;
+	 myfilesystem.superblock->magic_number = MAGIC_N;
 	 //NUMERO DE BLOQUES TOTAL
-	 superblock->bitmapb_offset = BLOCK_SIZE;
-	 superblock->block_size = BLOCK_SIZE;
-	 superblock->num_blocks =  statbuf.st_size/BLOCK_SIZE;//NUMERO DE BLOQUES TOTAL
-	 superblock->num_inodes = superblock->num_blocks/4;
-	 if(superblock->num_blocks%BLOCK_SIZE == 0 && superblock->num_blocks != BLOCK_SIZE){
-		 superblock->bitmapi_offset = superblock->bitmapb_offset + BLOCK_SIZE*(superblock->num_blocks/BLOCK_SIZE);
+	 myfilesystem.superblock->block_size = BLOCK_SIZE;
+	 myfilesystem.superblock->num_blocks =  statbuf.st_size/BLOCK_SIZE;//NUMERO DE BLOQUES TOTAL
+
+	 //Para el número de bloques total que tenemos por inodo, vamos a multiplicarlo por el número de inodos que hay en un bloque
+	 myfilesystem.superblock->num_inodes = (myfilesystem.superblock->num_blocks/4)*(BLOCK_SIZE/sizeof(struct inode_fs));
+
+	 //****VAMOS A CALCULAR LOS OFFSET****//
+
+	 myfilesystem.superblock->bitmapb_offset = BLOCK_SIZE;
+
+	 if((myfilesystem.superblock->num_blocks/8)%BLOCK_SIZE == 0){ // -> NUMERO DE BLOQUES QUE VAMOS A OCUPAR
+		 myfilesystem.superblock->bitmapi_offset = myfilesystem.superblock->bitmapb_offset + (myfilesystem.superblock->num_blocks/8);
 	 }else{
-		 superblock->bitmapi_offset = superblock->bitmapb_offset + BLOCK_SIZE*(1+superblock->num_blocks/BLOCK_SIZE);
+		 myfilesystem.superblock->bitmapi_offset = myfilesystem.superblock->bitmapb_offset + (((myfilesystem.superblock->num_blocks/8)/BLOCK_SIZE)+1)*BLOCK_SIZE;
 	 }
 
 	 //Guardamos los inodos en un array de caracteres, cada caracter es 1 byte que representa a 8 inodos
-	 if((superblock->num_inodes/8)%BLOCK_SIZE == 0 && superblock->num_inodes != BLOCK_SIZE){
-		 superblock->offset_inodos = superblock->bitmapi_offset + (superblock->num_inodes/8)/BLOCK_SIZE;
+	 if((myfilesystem.superblock->num_inodes/8)%BLOCK_SIZE == 0){
+		 myfilesystem.superblock->offset_inodos = myfilesystem.superblock->bitmapi_offset + (myfilesystem.superblock->num_inodes/8);
 	 }else{
-		 superblock->offset_inodos = superblock->bitmapi_offset + (superblock->num_inodes/8)/BLOCK_SIZE+1;
+		 myfilesystem.superblock->offset_inodos = myfilesystem.superblock->bitmapi_offset + (((myfilesystem.superblock->num_inodes/8)/BLOCK_SIZE)+1) * BLOCK_SIZE;
 	 }
-	 if((sizeof(struct inode_fs) * superblock->num_inodes)%BLOCK_SIZE == 0){
-		 superblock->offset_bloques = superblock->offset_inodos + (sizeof(struct inode_fs) * superblock->num_inodes)/BLOCK_SIZE;
+	 if((sizeof(struct inode_fs) * myfilesystem.superblock->num_inodes)%BLOCK_SIZE == 0){
+		 myfilesystem.superblock->offset_bloques = myfilesystem.superblock->offset_inodos + (sizeof(struct inode_fs) * myfilesystem.superblock->num_inodes);
 	 }else{
-		 superblock->offset_bloques = superblock->offset_inodos + ((sizeof(struct inode_fs) * superblock->num_inodes)/BLOCK_SIZE) +1;
+		 myfilesystem.superblock->offset_bloques = myfilesystem.superblock->offset_inodos + (((sizeof(struct inode_fs) * myfilesystem.superblock->num_inodes)/BLOCK_SIZE) +1)*BLOCK_SIZE;
 	 }
 
 	 //movemos el puntero del fichero a donde apunta el offset
-	 unsigned long bitmap_offset = superblock->bitmapb_offset;
-	 if(superblock->num_blocks%8 != 0){
-		 nb = superblock->num_blocks/8 +1;
-	 }
-	 bitmapb.array = (char*) mmap(NULL,nb,PROT_WRITE|PROT_READ, MAP_SHARED,file, bitmap_offset);
-	 bitmapb.size = superblock->num_blocks/8;
+	 unsigned long bitmap_offset = myfilesystem.superblock->bitmapb_offset;
+	 unsigned long numb = myfilesystem.superblock->num_blocks/8, numi = myfilesystem.superblock->num_inodes/8;
+	 myfilesystem.block_bitmap.array = (unsigned char*) mmap(NULL,numb,PROT_WRITE|PROT_READ, MAP_SHARED,file, bitmap_offset);
+	 myfilesystem.block_bitmap.size = numb;
+
+	 //**Pruebas bitmapb**//
 	 //Seteamos a 1 tantos bits como bloques tengamos ocupados
 	 //Bloques ocupados: (1)Superbloque + ()Bitmapb + ()Bitmapi + num_inodos
-	 uint8_t i;
-	 uint8_t numb = superblock->num_blocks/8, numi = superblock->num_inodes/8;
+
+	 unsigned long i;
+	 unsigned long nb, ni;
 	 if(numb%BLOCK_SIZE!= 0){
-	 	numb = numb/BLOCK_SIZE + 1;
-	 }
+	 	 	 nb = (numb/BLOCK_SIZE + 1);
+	 	 }
 	 if(numi%BLOCK_SIZE != 0){
-	 	numi = numi/BLOCK_SIZE + 1;
+	 	 ni = (numi/BLOCK_SIZE + 1);
 	 }
-	 unsigned long bloques_ocupados = 1 + numb+ numi + superblock->num_inodes;
+	 unsigned long bloques_ocupados = 1 + nb + ni + myfilesystem.superblock->num_inodes;
 	 int control = 0;
 	 while(i < bloques_ocupados && (control !=-1)){
-		 control = setBit(&bitmapb);
+		 control = free_bit(&(myfilesystem.block_bitmap));
 		 if(control == -1){
 			 printf("Error, no hay espacio");
 			 exit(EXIT_FAILURE);
 		 }
 		 i++;
 	 }
+	 //**FIN DE PRUEBAS**//
 
-	 bitmap_offset = superblock->bitmapi_offset;
+	 bitmap_offset = myfilesystem.superblock->bitmapi_offset;
 
-	 nb = 1;
-	 if(superblock->num_inodes%8 != 0){
-	 	nb = superblock->num_inodes/8 +1;
-	 }
-	 bitmapi.array = (unsigned char*) mmap(NULL,nb,PROT_WRITE|PROT_READ, MAP_SHARED,file, bitmap_offset);
-	 bitmapi.size = superblock->num_inodes/8;
-	 i = 0;
-	setBit(&bitmapi);
+	 myfilesystem.inode_bitmap.array = (unsigned char*) mmap(NULL,numi,PROT_WRITE|PROT_READ, MAP_SHARED,file, bitmap_offset);
+	 myfilesystem.inode_bitmap.size = numi;
 
+	 //**MAS TESTSS**//
+//	i = 0;
+//	setBit(private_data.inode_bitmap);
+	//**FIN TESTS**//
 
-	 inodos = (struct inode_fs*) mmap(NULL,superblock->offset_bloques-superblock->offset_inodos, PROT_WRITE|PROT_READ, MAP_SHARED,file, superblock->offset_inodos);
-	 bloques = (struct block_t*) mmap(NULL,statbuf.st_size-superblock->offset_bloques, PROT_WRITE|PROT_READ, MAP_SHARED,file, superblock->offset_bloques);
+	 myfilesystem.inode = (struct inode_fs*) mmap(NULL,myfilesystem.superblock->bitmapb_offset-myfilesystem.superblock->bitmapi_offset, PROT_WRITE|PROT_READ, MAP_SHARED,file, myfilesystem.superblock->offset_inodos); // @suppress("Field cannot be resolved")
+	 myfilesystem.block = (block_t*) mmap(NULL,statbuf.st_size-myfilesystem.superblock->offset_bloques, PROT_WRITE|PROT_READ, MAP_SHARED,file, myfilesystem.superblock->offset_bloques);
 
 	printf("Todo guardado\n");
 
 	close(file);
 
 	file = open("filesystem.txt", O_CREAT | O_RDWR, 0664);
-	printf("%lu\n,%lu\n,%lu\n,%lu\n, %lu\n, %lu\n",superblock->magic_number, superblock->num_blocks, superblock->num_inodes, superblock->block_size, superblock->bitmapb_offset, superblock->bitmapi_offset);
-//	fflush(stdout);
-//	for(i = 0; i < bitmapb.size; i++){
-//		printf("%c", bitmapb.array[i]);
-//	}
-//	fflush(stdout);
-	for(i = 0; i < bitmapi.size; i++){
-			printf("%c", bitmapi.array[i]);
+	//printf("%lu\n,%lu\n,%lu\n,%lu\n, %lu\n, %lu\n",superblock->magic_number, superblock->num_blocks, superblock->num_inodes, superblock->block_size, superblock->bitmapb_offset, superblock->bitmapi_offset);
+	fflush(stdout);
+	for(i = 0; i < myfilesystem.block_bitmap.size; i++){
+		printf("%#x\n", myfilesystem.block_bitmap.array[i]);
 	}
+//	fflush(stdout);
+//	for(i = 0; i < bitmapi.size; i++){
+//			printf("%c", bitmapi.array[i]);
+//	}
 	fflush(stdout);
 	close(file);
 }
